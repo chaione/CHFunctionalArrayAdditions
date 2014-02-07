f9d4efb30e516a0286d454a111eaee1f151c8b7e
Terry Lewis <terrylewis@outlook.com>
2014-02-07 14:02:35 -0600

    initial commit

---
+++ CHFunctionalArrayAdditions/CHViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHViewController.h
+//  CHFunctionalArrayAdditions
+//
+//  Created by Terry Lewis II on 2/7/14.
+//  Copyright (c) 2014 Blue Plover Productions LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHViewController : UIViewController
+
+@end
